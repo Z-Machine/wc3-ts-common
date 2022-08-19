c56020ddd1bc04248302d796c2dd69f864362c82
Zed <Z-Machine@users.noreply.github.com>
2022-08-19 14:36:25 -0500

    asCondition

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -1,3 +1,4 @@
-// TODO: move these to a filter file?
 export type AnyFilter = boolexpr | (() => boolean);
+
 export const asFilter = (filter: AnyFilter) => (typeof filter === "function" ? Filter(filter) : filter);
+export const asCondition = (filter: AnyFilter) => (typeof filter === "function" ? Condition(filter) : filter);
