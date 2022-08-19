f9104fb12efefc99b01aa828e4ffc7526ac80b49
Zed <Z-Machine@users.noreply.github.com>
2022-08-19 11:25:27 -0500

    split dialog files

--- src/handles/dialog.ts
+++ src/handles/dialog.ts
@@ -1,3 +1,4 @@
+import DialogButton from "./dialogbutton";
 import Handle, { IDestroyable } from "./handle";
 import type MapPlayer from "./player";
 
@@ -66,72 +67,6 @@ export default class Dialog extends Handle implements IDestroyable {
 
         this.initHandle = handle;
         o = new Dialog();
-        this.initHandle = undefined;
-
-        return o;
-    }
-}
-
-export class DialogButton extends Handle {
-    protected static override map: WeakMap<handle, DialogButton>;
-    public declare readonly handle: button;
-
-    /**
-     * @param whichDialog required
-     * @param text required
-     * @param hotkey optional
-     * @param quit optional
-     * @param score optional
-     */
-    public constructor(whichDialog?: Dialog, text?: string, hotkey?: number, quit?: boolean, score?: boolean);
-    public constructor(
-        whichDialog: Dialog,
-        text: string,
-        hotkey: number = 0,
-        quit: boolean = false,
-        score: boolean = false
-    ) {
-        if (DialogButton.initHandle) {
-            super(DialogButton.initHandle);
-            DialogButton.map.set(this.handle, this);
-            return;
-        }
-
-        if (whichDialog === undefined || text === undefined) {
-            error(`DialogButton.Constructor missing required parameters.`, 3);
-        }
-
-        const handle = quit
-            ? DialogAddButton(whichDialog.handle, text, hotkey)
-            : DialogAddQuitButton(whichDialog.handle, score, text, hotkey);
-
-        if (handle !== undefined) {
-            super(handle);
-            DialogButton.map.set(handle, this);
-            return;
-        }
-
-        error(`Failed to create button handle.`, 3);
-    }
-
-    public override isValid(): this is Dialog {
-        return GetHandleId(this.handle) !== 0;
-    }
-
-    public static fromEvent() {
-        return this.fromHandle(GetClickedButton());
-    }
-
-    public static fromHandle(handle?: button) {
-        return handle ? this.getObject(handle) : undefined;
-    }
-
-    protected static override getObject(handle: button) {
-        let o = this.map.get(handle);
-        if (o !== undefined) return o;
-
-        this.initHandle = handle;
-        o = new DialogButton();
         this.initHandle = undefined;
 
         return o;
