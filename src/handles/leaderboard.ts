e38bc957d38797cec51e7c81c3db13c47c902f98
Zed <Z-Machine@users.noreply.github.com>
2022-08-19 20:11:23 -0500

    more leaderboard

--- src/handles/leaderboard.ts
+++ src/handles/leaderboard.ts
@@ -1,5 +1,5 @@
 import Handle, { IDestroyable } from "./handle";
-import MapPlayer from "./player";
+import type MapPlayer from "./player";
 
 export default class Leaderboard extends Handle implements IDestroyable {
     protected static override map: WeakMap<leaderboard, Leaderboard>;
@@ -12,7 +12,7 @@ export default class Leaderboard extends Handle implements IDestroyable {
         }
 
         const handle = CreateLeaderboard();
-        if (handle) {
+        if (handle !== undefined) {
             super(handle);
             Leaderboard.map.set(handle, this);
             return;
@@ -29,6 +29,102 @@ export default class Leaderboard extends Handle implements IDestroyable {
     public destroy() {
         DestroyLeaderboard(this.handle);
         Leaderboard.map.delete(this.handle);
+    }
+
+    public clear() {
+        LeaderboardClear(this.handle);
+    }
+
+    public addItem(label: string, value: number, whichPlayer: MapPlayer) {
+        LeaderboardAddItem(this.handle, label, value, whichPlayer.handle);
+    }
+
+    public removeItem(index: number) {
+        LeaderboardRemoveItem(this.handle, index);
+    }
+
+    public get display() {
+        return IsLeaderboardDisplayed(this.handle);
+    }
+
+    public set display(b: boolean) {
+        LeaderboardDisplay(this.handle, b);
+    }
+
+    public get itemCount() {
+        return LeaderboardGetItemCount(this.handle);
+    }
+
+    public set itemCount(v: number) {
+        LeaderboardSetSizeByItemCount(this.handle, v);
+    }
+
+    public get label() {
+        return LeaderboardGetLabelText(this.handle) ?? "";
+    }
+
+    public set label(value: string) {
+        LeaderboardSetLabel(this.handle, value);
+    }
+
+    public getPlayerIndex(whichPlayer: MapPlayer) {
+        return LeaderboardGetPlayerIndex(this.handle, whichPlayer.handle);
+    }
+
+    public hasPlayerItem(whichPlayer: MapPlayer) {
+        LeaderboardHasPlayerItem(this.handle, whichPlayer.handle);
+    }
+
+    public removePlayerItem(whichPlayer: MapPlayer) {
+        LeaderboardRemovePlayerItem(this.handle, whichPlayer.handle);
+    }
+
+    public setItemLabel(whichItem: number, label: string) {
+        LeaderboardSetItemLabel(this.handle, whichItem, label);
+    }
+
+    public setItemLabelColor(whichItem: number, red: number, green: number, blue: number, alpha: number) {
+        LeaderboardSetItemLabelColor(this.handle, whichItem, red, green, blue, alpha);
+    }
+
+    public setItemStyle(whichItem: number, showLabel = true, showValues = true, showIcons = true) {
+        LeaderboardSetItemStyle(this.handle, whichItem, showLabel, showValues, showIcons);
+    }
+
+    public setItemValue(whichItem: number, value: number) {
+        LeaderboardSetItemValue(this.handle, whichItem, value);
+    }
+
+    public setItemValueColor(whichItem: number, red: number, green: number, blue: number, alpha: number) {
+        LeaderboardSetItemValueColor(this.handle, whichItem, red, green, blue, alpha);
+    }
+
+    public setLabelColor(red: number, green: number, blue: number, alpha: number) {
+        LeaderboardSetLabelColor(this.handle, red, green, blue, alpha);
+    }
+
+    public setPlayerBoard(whichPlayer: MapPlayer) {
+        PlayerSetLeaderboard(whichPlayer.handle, this.handle);
+    }
+
+    public setStyle(showLabel = true, showNames = true, showValues = true, showIcons = true) {
+        LeaderboardSetStyle(this.handle, showLabel, showNames, showValues, showIcons);
+    }
+
+    public setValueColor(red: number, green: number, blue: number, alpha: number) {
+        LeaderboardSetValueColor(this.handle, red, green, blue, alpha);
+    }
+
+    public sortByLabel(ascending = true) {
+        LeaderboardSortItemsByLabel(this.handle, ascending);
+    }
+
+    public sortByPlayer(ascending = true) {
+        LeaderboardSortItemsByPlayer(this.handle, ascending);
+    }
+
+    public sortByValue(ascending = true) {
+        LeaderboardSortItemsByValue(this.handle, ascending);
     }
 
     public static fromPlayer(whichPlayer: MapPlayer) {
