8c6973e390a52f3678014165bf50d6cb5c52de21
Zed <Z-Machine@users.noreply.github.com>
2022-08-20 23:33:53 -0500

    temp polyfill for missing natives

---
+++ src/types/polyfill.d.ts
@@ -0,0 +1,8 @@
+declare function BlzGetAbilityId(whichAbility: ability): number;
+declare function BlzQueueNeutralPointOrderById(
+    forWhichPlayer: player,
+    neutralStructure: unit,
+    unitId: number,
+    x: number,
+    y: number
+): boolean;
